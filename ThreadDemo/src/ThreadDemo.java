138add701aa27128ccac8da111cb1ed2ea25f4cf
Shree Devi Yadam <shree.yadam@gmail.com>
2023-03-16 21:10:52 -0700

    Demo creation of thread by implementing Runnable interface

---
+++ ThreadDemo/src/ThreadDemo.java
@@ -0,0 +1,17 @@
+public class ThreadDemo {
+    public static void main(String[] args){
+        NewThread nt = new NewThread(); // create a new thread
+
+        nt.t.start(); // start the thread
+
+        try{
+            for (int i = 5; i > 0; i--) {
+                System.out.println("Main Thread: " + i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted.");
+        }
+        System.out.println("Main thread exiting.");
+    }
+}
